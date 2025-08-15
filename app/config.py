52974c216594e76218dd3809f2462d3840525a90
Stepan Fedosov <zollidan@yandex.ru>
2025-08-15 05:05:58 +0300

    refactor: simplify Settings class by removing dotenv loading and environment variable assignments

--- app/config.py
+++ app/config.py
@@ -1,15 +1,12 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Settings(BaseSettings):
-    BUCKET_NAME: str = os.getenv("BUCKET_NAME")
-    ENDPOINT: str = os.getenv("ENDPOINT")
-    ACCESS_KEY: str = os.getenv("ACCESS_KEY")
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    BUCKET_NAME: str
+    ENDPOINT: str
+    ACCESS_KEY: str
+    SECRET_KEY: str
     MAX_FILE_SIZE: int = 10 * 1024 * 1024
     ALLOWED_TYPES: list[str] = ["image/jpeg", "image/png", "application/pdf"]
     ORIGINS: list[str] = [
@@ -22,10 +19,10 @@ class Settings(BaseSettings):
     ]
     S3_PUBLIC_URL: str = "/s3/file"
 
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), "..", ".env")
-    )
+    # model_config = SettingsConfigDict(
+    #     env_file=os.path.join(os.path.dirname(
+    #         os.path.abspath(__file__)), "..", ".env")
+    # )
 
 
 settings = Settings()
