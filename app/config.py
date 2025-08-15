f62115281805f80f9a53119731b5f724bae3fd62
Stepan Fedosov <zollidan@yandex.ru>
2025-08-15 17:34:22 +0300

    fix config

--- app/config.py
+++ app/config.py
@@ -12,14 +12,15 @@ class Settings(BaseSettings):
     ORIGINS: list[str] = [
         "http://localhost:5173",
         "http://localhost:80",
-        "http://localhost:8000"
+        "http://localhost",
+        "https://zollidan-image-framer-4c80.twc1.net"
     ]
     S3_PUBLIC_URL: str = "/s3/file"
 
-    # model_config = SettingsConfigDict(
-    #     env_file=os.path.join(os.path.dirname(
-    #         os.path.abspath(__file__)), "..", ".env")
-    # )
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "..", ".env")
+    )
 
 
 settings = Settings()
