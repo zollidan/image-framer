34452a2456764eb300a6bb950c48d74279ba885d
Stepan Fedosov <zollidan@yandex.ru>
2025-08-15 04:53:54 +0300

    fix: load environment variables in Settings class

--- app/config.py
+++ app/config.py
@@ -1,12 +1,15 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    BUCKET_NAME: str = ""
-    ENDPOINT: str = ""
-    ACCESS_KEY: str = ""
-    SECRET_KEY: str = ""
+    BUCKET_NAME: str = os.getenv("BUCKET_NAME")
+    ENDPOINT: str = os.getenv("ENDPOINT")
+    ACCESS_KEY: str = os.getenv("ACCESS_KEY")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     MAX_FILE_SIZE: int = 10 * 1024 * 1024
     ALLOWED_TYPES: list[str] = ["image/jpeg", "image/png", "application/pdf"]
     ORIGINS: list[str] = [
